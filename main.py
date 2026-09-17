627df8afdbd80256097aae765f49cf0e05fd030b
kathhee <francakathellin@gmail.com>
2026-09-17 15:34:23 -0300

    Login alterado

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
+from flask import Flask, render_template
 import os
-
-from flask import render_template, Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     nome = 'icoma.com.br'
-    return render_template('index.html', site = nome)
+    return render_template('index.html', site=nome)
 
-@app.route("/login")
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template('login/login.html')
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    return render_template('login/register.html')
+
+@app.route('/recovery')
+def recovery():
+    return render_template('login/recovery.html')
+
 def main():
-    app.run(host="0.0.0.0", port = int(os.environ.get("PORT", 10000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
 
 if __name__ == "__main__":
     main()
